0c82dce72a4ef1a75f8ab1e2ba3ed85e59898cce
dragonjjr <43954443+dragonjjr@users.noreply.github.com>
2022-08-26 23:35:43 +0700

    Add write log

--- ClientMonitoring/src/client/FolderMonitoring.java
+++ ClientMonitoring/src/client/FolderMonitoring.java
@@ -2,60 +2,83 @@ package client;
 
 import java.io.*;
 import java.nio.file.*;
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 import static java.nio.file.StandardWatchEventKinds.*;
 
 public class FolderMonitoring extends Thread {
 
-    private DataOutputStream dos;
-    private DataInputStream dis;
+	private DataOutputStream dos;
+	private DataInputStream dis;
 
-    public DataOutputStream getDos() {
-        return dos;
-    }
+	Logger logger = Logger.getLogger("MyLog");
+	FileHandler fh;
 
-    public void setDos(DataOutputStream dos) {
-        this.dos = dos;
-    }
+	public DataOutputStream getDos() {
+		return dos;
+	}
 
-    public DataInputStream getDis() {
-        return dis;
-    }
+	public void setDos(DataOutputStream dos) {
+		this.dos = dos;
+	}
 
-    public void setDis(DataInputStream dis) {
-        this.dis = dis;
-    }
-    public FolderMonitoring(DataInputStream dis, DataOutputStream dos)
-    {
-        this.dos = dos;
-        this.dis = dis;
-    }
+	public DataInputStream getDis() {
+		return dis;
+	}
 
-    @Override
-    public void run() {
-        try {
-            //Client connect server
-            dos.writeUTF("");
+	public void setDis(DataInputStream dis) {
+		this.dis = dis;
+	}
 
-            //Mornitoring folder
-            FileSystem fs = FileSystems.getDefault();
-            WatchService ws = fs.newWatchService();
-            Path pTemp = Paths.get("D:/Monitoring");
-            pTemp.register(ws, new WatchEvent.Kind[]{ENTRY_MODIFY, ENTRY_CREATE, ENTRY_DELETE});
-            while (true) {
-                WatchKey k = ws.take();
-                for (WatchEvent<?> e : k.pollEvents()) {
-                    Object c = e.context();
-                    dos.writeUTF(String.valueOf(e.kind()));
-                    dos.writeUTF(String.valueOf(e.count()));
-                    dos.writeUTF(String.valueOf(c));
-                    System.out.printf("%s %d %s\n", e.kind(), e.count(), c);
-                }
-                k.reset();
-            }
-        }catch (Exception e)
-        {
-            System.out.println(e.getMessage());
-        }
-    }
+	public FolderMonitoring(DataInputStream dis, DataOutputStream dos) {
+		this.dos = dos;
+		this.dis = dis;
+	}
+
+	@Override
+	public void run() {
+		try {
+			// Client connect server
+			dos.writeUTF("");
+
+			// Mornitoring folder
+			FileSystem fs = FileSystems.getDefault();
+			WatchService ws = fs.newWatchService();
+			Path pTemp = Paths.get("D:/Monitoring");
+			pTemp.register(ws, new WatchEvent.Kind[] { ENTRY_MODIFY, ENTRY_CREATE, ENTRY_DELETE });
+			while (true) {
+				WatchKey k = ws.take();
+				for (WatchEvent<?> e : k.pollEvents()) {
+					Object c = e.context();
+					dos.writeUTF(String.valueOf(e.kind()));
+					dos.writeUTF(String.valueOf(e.count()));
+					dos.writeUTF(String.valueOf(c));
+					System.out.printf("%s %d %s\n", e.kind(), e.count(), c);
+
+					// write log
+					try {
+
+						// This block configure the logger with handler and formatter
+						fh = new FileHandler("Monitoring.log");
+						logger.addHandler(fh);
+						SimpleFormatter formatter = new SimpleFormatter();
+						fh.setFormatter(formatter);
+
+						// the following statement is used to log any messages
+						logger.info(e.kind() + " " + e.count() + " " + c);
+
+					} catch (SecurityException er) {
+						er.printStackTrace();
+					} catch (IOException er) {
+						er.printStackTrace();
+					}
+				}
+				k.reset();
+			}
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
 }
--- ClientMonitoring/src/server/Server.java
+++ ClientMonitoring/src/server/Server.java
@@ -1,77 +1,95 @@
 package server;
+
 import client.Client;
 
 import java.io.IOException;
 import java.net.*;
 import java.util.ArrayList;
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
+public class Server {
 
-public class Server{
+	Logger logger = Logger.getLogger("MyLog");
+	FileHandler fh;
 
-    private String IP;
-    private int Port;
-    ServerSocket serverSocket;
-    private ArrayList<Client> listClient;
+	private String IP;
+	private int Port;
+	ServerSocket serverSocket;
+	private ArrayList<Client> listClient;
 
-    public ArrayList<Client> getListClient() {
-        return listClient;
-    }
+	public ArrayList<Client> getListClient() {
+		return listClient;
+	}
 
-    public String getIP()
-    {
-        return IP;
-    }
+	public String getIP() {
+		return IP;
+	}
 
-    public int getPort()
-    {
-        return Port;
-    }
+	public int getPort() {
+		return Port;
+	}
 
+	public Server() throws UnknownHostException {
+		IP = InetAddress.getLocalHost().getHostAddress();
+		Port = 6000;
+	}
 
-    public Server() throws UnknownHostException {
-            IP = InetAddress.getLocalHost().getHostAddress();
-            Port = 6000;
-    }
+	public void startServer() throws IOException {
+		serverSocket = new ServerSocket(Port);
+		listClient = new ArrayList<Client>();
+		System.out.println("Server is listening on port " + Port);
 
-    public void startServer() throws IOException {
-        serverSocket = new ServerSocket(Port);
-        listClient = new ArrayList<Client>();
-        System.out.println("Server is listening on port " + Port);
+		// Tạo thread riêng cho server
+		Thread t = new Thread() {
+			@Override
+			public void run() {
+				while (serverSocket != null && !serverSocket.isClosed()) {
+					try {
+						Socket socket = serverSocket.accept();
 
+						System.out.println("New client connected " + socket);
+						// write log.
 
-        //Tạo thread riêng cho server
-        Thread t = new Thread(){
-            @Override
-            public void run(){
-                while (serverSocket != null && !serverSocket.isClosed()) {
-                    try {
-                        Socket socket = serverSocket.accept();
+						try {
 
-                        System.out.println("New client connected " + socket);
+							// This block configure the logger with handler and formatter
+							fh = new FileHandler("Monitoring.log");
+							logger.addHandler(fh);
+							SimpleFormatter formatter = new SimpleFormatter();
+							fh.setFormatter(formatter);
 
-                        //Tạo thread cho từng client
-                        ClientHandle clientHandler = new ClientHandle(socket);
-                        clientHandler.start();
+							// the following statement is used to log any messages
+							logger.info("New client connected " + socket);
 
-                    } catch (IOException e) {
-                        break;
-                    }
-                }
-            }
-        };
-        t.start();
-    }
+						} catch (SecurityException er) {
+							er.printStackTrace();
+						} catch (IOException er) {
+							er.printStackTrace();
+						}
 
-    public void CloseServer() throws IOException {
-        serverSocket.close();
-    }
+						// Tạo thread cho từng client
+						ClientHandle clientHandler = new ClientHandle(socket);
+						clientHandler.start();
 
+					} catch (IOException e) {
+						break;
+					}
+				}
+			}
+		};
+		t.start();
+	}
 
-    public void addNewClient(Client client)
-    {
-        this.listClient.add(client);
-        System.out.println(this.listClient);
-        Main.getGuiServer().updateList_Client();
-    }
+	public void CloseServer() throws IOException {
+		serverSocket.close();
+	}
+
+	public void addNewClient(Client client) {
+		this.listClient.add(client);
+		System.out.println(this.listClient);
+		Main.getGuiServer().updateList_Client();
+	}
 
 }
